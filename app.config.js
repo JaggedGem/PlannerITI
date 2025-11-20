4c77cd5f088b45f3d8cb302bc861c7142faef4c9
JaggedGem <orletchi.bogdan2009@gmail.com>
2025-11-20 18:35:46 +0200

    fix version not being updated

--- app.config.js
+++ app.config.js
@@ -29,7 +29,7 @@ module.exports = () => {
     expo: {
       name: "PlannerITI",
       slug: "PlannerITI",
-      version: "1.0.1",
+      version: "1.1.1",
       orientation: "portrait",
       icon: "assets/images/icon.png",
       scheme: "myapp",
